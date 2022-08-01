16daee6f1268c1dafd482755b89afdf766425eb9
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-01 19:45:45 +0300

    Test category user_id is valid

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -38,5 +38,31 @@ RSpec.describe Category, type: :model do
         expect(category).to_not be_valid
       end
     end
+
+    context "when user_id is valid" do
+      it "is invalid user_id is not nil" do
+        category = build(:category)
+        expect(category).to be_valid
+      end
+
+      it "is invalid user_id is unique" do
+        create(:category)
+        category = build(:category)
+        expect(category).to be_valid
+      end
+    end
+
+    context "is invalid user_id is invalid" do
+      it "is invalid when user_id is nil" do
+        category = build(:category, user_id: nil)
+        expect(category).to be_invalid
+      end
+
+      it "is invalid when user_id is not unique" do
+        create(:category)
+        category = build(:category)
+        expect(category).to be_invalid
+      end
+    end
   end
 end
