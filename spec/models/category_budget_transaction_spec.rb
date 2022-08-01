3f3b9b4666f96b46513dd0d6588e8429bf04f919
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-01 20:37:15 +0300

    Test CategoryBudgetTransaction for invalidity

--- spec/models/category_budget_transaction_spec.rb
+++ spec/models/category_budget_transaction_spec.rb
@@ -40,5 +40,37 @@ RSpec.describe CategoryBudgetTransaction, type: :model do
         expect(category_budget_transaction).to be_valid
       end
     end
+
+    context "when budget_transaction is invalid" do
+      it "is invalid if budget_transaction is not specified" do
+        category_budget_transaction = build(:category_budget_transaction, budget_transaction: nil)
+        expect(category_budget_transaction).to be_invalid
+      end
+
+      it "is invalid if budget_transaction is not a number" do
+        category_budget_transaction = build(:category_budget_transaction, budget_transaction: "not a number")
+        expect(category_budget_transaction).to be_invalid
+      end
+
+      it "is invalid if budget_transaction is negative" do
+        category_budget_transaction = build(:category_budget_transaction, budget_transaction: -1)
+        expect(category_budget_transaction).to be_invalid
+      end
+
+      it "is invalid if category is not specified" do
+        category_budget_transaction = build(:category_budget_transaction, category: nil)
+        expect(category_budget_transaction).to be_invalid
+      end
+
+      it "is invalid if category is not a number" do
+        category_budget_transaction = build(:category_budget_transaction, category: "not a number")
+        expect(category_budget_transaction).to be_invalid
+      end
+
+      it "is invalid if category is negative" do
+        category_budget_transaction = build(:category_budget_transaction, category: -1)
+        expect(category_budget_transaction).to be_invalid
+      end
+    end
   end
 end
