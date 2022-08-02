6161727102e7d9da0b7e960024aae7c9148ade19
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-02 23:00:09 +0300

    Create methods for categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,27 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(user: current_user, name: category_params[:name])
+    if @category.save
+      flash[:notice] = "Category created successfully"
+      redirect_to categories_path
+    else
+      error = @category.errors.full_messages.join(", ")
+      flash[:alert] = error
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
