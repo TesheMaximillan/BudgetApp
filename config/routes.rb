84a8a1a50ff4784a02e1b292c601f403cac6ee0b
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-02 22:46:19 +0300

    Create routes for categories and budget_transactions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/create'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -6,8 +9,8 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   authenticated :user do
-    resources :categories, param: :user do
-      resources :budget_transactions
+    resources :categories, only: [:index, :create, :new] do
+      resources :budget_transactions, only: [:index, :create, :new]
     end
   end
 end
