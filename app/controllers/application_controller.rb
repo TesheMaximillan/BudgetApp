017de605096648ba3551f060cceb06b114c768ff
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-02 12:59:22 +0300

    Add email confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :config_devise_params, if: :devise_controller?
   layout :layout_by_resource
+
+  protected
+
+  def config_devise_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password password_confirmation])
+  end
 
   private
 
