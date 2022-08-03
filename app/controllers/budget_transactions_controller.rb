87f65f0a139682171ca76af2102d72e13e8112de
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-04 02:32:31 +0300

    Modify budget_transactions action

--- app/controllers/budget_transactions_controller.rb
+++ app/controllers/budget_transactions_controller.rb
@@ -10,18 +10,13 @@ class BudgetTransactionsController < ApplicationController
 
   def create
     @category = Category.find(params[:category_id])
-    @budget_transaction = BudgetTransaction.new(user: current_user, name: budget_transaction_params[:name], amount: budget_transaction_params[:amount])
+    @budget_transaction = BudgetTransaction.new(name: params[:name], amount: params[:amount])
+    @budget_transaction.user_id = current_user.id
     if @budget_transaction.save
       @category.budget_transactions << @budget_transaction
-      redirect to category_budget_transactions_path(category_id: @category.id), notice: 'Budget transaction was successfully created.'
+      redirect_to category_budget_transactions_path(category_id: @category.id), notice: 'Budget transaction was successfully created.'
     else
       render :new, alert: 'Budget transaction was not created.'
     end
-  end
-
-  private
-
-  def budget_transaction_params
-    params.require(:budget_transaction).permit(:name, :amount)
   end
 end
