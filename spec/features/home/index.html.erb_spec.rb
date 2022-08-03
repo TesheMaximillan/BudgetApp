94e6944906a394968d5c499de07c265d2755f46b
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-03 19:53:47 +0300

    Create integration test for home page

--- spec/features/home/index.html.erb_spec.rb
+++ spec/features/home/index.html.erb_spec.rb
@@ -1,10 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe "home/index", type: :feature do
-  describe "displays the Application name" do
+  describe "displays the home page" do
     it "displays the name of the app" do
       visit(root_path)
       expect(page).to have_content("Budget App")
+    end
+
+    it "Login button is present" do
+      visit(root_path)
+      expect(page).to have_content("LOG IN")
+    end
+
+    it "Login button Link to login page" do
+      visit(root_path)
+      expect(page).to have_link("LOG IN", href: new_user_session_path)
+    end
+
+    it "Signup button is present" do
+      visit(root_path)
+      expect(page).to have_content("SIGN UP")
+    end
+
+    it "Signup button Link to signup page" do
+      visit(root_path)
+      expect(page).to have_link("SIGN UP", href: new_user_registration_path)
+    end
+  end
+
+  describe "displays the SIGN IN and SIGN UP page" do
+    it "displays the header title LOGUN" do
+      visit(new_user_session_path)
+      expect(page).to have_content("LOGIN")
+    end
+
+    it "displays the header title SIGNUP" do
+      visit(new_user_registration_path)
+      expect(page).to have_content("SIGNUP")
     end
   end
 end
