2713eb06460a72c1a3833e88c2645fa7252e6e0b
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-04 22:19:02 +0300

    Add feature test for create icon

--- spec/features/categories/new.html.erb_spec.rb
+++ spec/features/categories/new.html.erb_spec.rb
@@ -15,4 +15,21 @@ RSpec.describe "categories/new.html.erb", type: :feature do
     visit new_category_path
     expect(page).to have_button('Create')
   end
+
+  it "Click Create Button with blank fields" do
+    user = create(:user)
+    sign_in user
+    visit new_category_path
+    click_button 'Create'
+    expect(page).to have_current_path(categories_path)
+  end
+
+  it "Click Create Button with valid fields" do
+    user = create(:user)
+    sign_in user
+    visit new_category_path
+    fill_in 'Name', with: 'Test Category'
+    click_button 'Create'
+    expect(page).to have_current_path(categories_path)
+  end
 end
