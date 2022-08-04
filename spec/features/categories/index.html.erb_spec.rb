49fe095e759e6e08444cafd86c62536748ac92f5
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-04 20:56:05 +0300

    Test Click event for Add Category

--- spec/features/categories/index.html.erb_spec.rb
+++ spec/features/categories/index.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "categories/index.html.erb", type: :feature do
-  before(:example) do
+  before(:each) do
     @user = build(:user)
     sign_in @user
   end
@@ -14,5 +14,21 @@ RSpec.describe "categories/index.html.erb", type: :feature do
   it "Sees Add Category Link" do
     visit categories_path
     expect(page).to have_link('Add Category')
+  end
+
+  it "Click link Add Category" do
+    user = create(:user)
+    sign_in user
+    visit categories_path
+    click_link 'Add Category'
+    expect(page).to have_current_path(new_category_path)
+  end
+
+  it "Click link Add Category" do
+    user = create(:user)
+    sign_in user
+    visit categories_path
+    click_link 'Add Category'
+    expect(page).to have_content('NEW CATEGORY')
   end
 end
