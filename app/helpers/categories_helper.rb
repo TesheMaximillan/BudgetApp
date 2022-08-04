06aafa4ef44143e8bb4088ff89bb30cf36627481
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-04 22:19:56 +0300

    Add default image for category

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,9 @@
 module CategoriesHelper
+  def group_avatar(icon)
+    if icon.present?
+      image_tag(url_for(icon), alt: 'icon', class: 'thumbnail icon-img', width: '64', height: '64')
+    else
+      image_tag('default_icon.jpg', alt: 'icon', class: 'img')
+    end
+  end
 end
