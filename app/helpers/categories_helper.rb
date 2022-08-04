f7ef97111ce9905ab140660b9f5bc167e8b2cc97
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-04 23:15:33 +0300

    Test Add Transasction button to navigate to new page

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -3,7 +3,7 @@ module CategoriesHelper
     if icon.present?
       image_tag(url_for(icon), alt: 'icon', class: 'img')
     else
-      image_tag('default_icon.jpg', alt: 'icon', class: 'img')
+      image_tag('default_icon.png', alt: 'icon', class: 'img')
     end
   end
 end
--- spec/features/budget_transactions/index.html.erb_spec.rb
+++ spec/features/budget_transactions/index.html.erb_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe "categories/index.html.erb", type: :feature do
     visit(category_budget_transactions_path(@category.id))
     expect(page).to have_link('Add Transaction')
   end
+
+  it "Click link Add Transaction" do
+    user = create(:user)
+    sign_in user
+    visit category_budget_transactions_path(@category.id)
+    click_link 'Add Transaction'
+    expect(page).to have_current_path(new_category_budget_transaction_path(@category.id))
+  end
+
+  it "Click link Add Transaction" do
+    user = create(:user)
+    sign_in user
+    visit category_budget_transactions_path(@category.id)
+    click_link 'Add Transaction'
+    expect(page).to have_content('NEW TRANSACTION')
+  end
+
 end
