76817333a107ff3edc6ac92c5c461417b49ae470
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-04 20:03:09 +0300

    Add feature test for catagories#index

--- spec/features/categories/index.html.erb_spec.rb
+++ spec/features/categories/index.html.erb_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "categories/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "categories/index.html.erb", type: :feature do
+  before(:example) do
+    @user = build(:user)
+    sign_in @user
+  end
+
+  it "displays header title 'CATEGORIES'" do
+    visit(categories_path)
+    expect(page).to have_content('CATEGORIES')
+  end
+
+  it "Sees Add Category Link" do
+    visit categories_path
+    expect(page).to have_link('Add Category')
+  end
 end
