dc3b94cf0443d527e2ef023c8d5c97ad684f49cd
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-04 16:17:43 +0300

    Redirect to cateogries page when the user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,16 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password password_confirmation])
   end
 
+  protected
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User)
+      categories_path(resource)
+    else
+      super
+    end
+  end
+
   private
 
   def layout_by_resource
