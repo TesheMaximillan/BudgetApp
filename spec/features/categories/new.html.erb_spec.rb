f9f0fbab7c0a6139db831067f3c7aae71d1ca8f1
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-04 23:10:28 +0300

    Test Create button is clicked with image and without image

--- spec/features/categories/new.html.erb_spec.rb
+++ spec/features/categories/new.html.erb_spec.rb
@@ -21,14 +21,24 @@ RSpec.describe "categories/new.html.erb", type: :feature do
     sign_in user
     visit new_category_path
     click_button 'Create'
-    expect(page).to have_current_path(categories_path)
+    expect(page).to have_current_path(new_category_path)
   end
 
-  it "Click Create Button with valid fields" do
+  it "Click Create Button withouy image fields" do
     user = create(:user)
     sign_in user
     visit new_category_path
     fill_in 'Name', with: 'Test Category'
+    click_button 'Create'
+    expect(page).to have_current_path(new_category_path)
+  end
+
+  it "Click Create Button with image fields" do
+    user = create(:user)
+    sign_in user
+    visit new_category_path
+    fill_in 'Name', with: 'Test Category'
+    attach_file 'Icon', Rails.root + 'app/assets/images/default_icon.png'
     click_button 'Create'
     expect(page).to have_current_path(categories_path)
   end
