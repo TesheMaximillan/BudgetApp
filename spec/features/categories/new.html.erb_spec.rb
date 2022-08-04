ce89427d8c31f2eb6a93e5a06576a1b9937ec558
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-04 20:16:16 +0300

    Add feature test for categories#new

--- spec/features/categories/new.html.erb_spec.rb
+++ spec/features/categories/new.html.erb_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "categories/new.html.erb", type: :feature do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:example) do
+    @user = build(:user)
+    sign_in @user
+  end
+
+  it "displays header title 'NEW CATEGORY'" do
+    visit(new_category_path)
+    expect(page).to have_content('NEW CATEGORY')
+  end
+
+  it "Sees Create Button" do
+    visit new_category_path
+    expect(page).to have_button('Create')
+  end
 end
