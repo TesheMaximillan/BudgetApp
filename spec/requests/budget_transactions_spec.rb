8b7d136ccc9dbfbec64d5127f9e81dcc72e5cc09
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-04 17:43:28 +0300

    Test budget_transactions controller

--- spec/requests/budget_transactions_spec.rb
+++ spec/requests/budget_transactions_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "BudgetTransactions", type: :request do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:example) do
+    @user= build(:user)
+    @category= build(:category)
+    @category.save
+    sign_in @user
+  end
+
+  describe "GET #index" do
+    it "returns http success" do
+      get category_budget_transactions_path(@category.id)
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the index template" do
+      get category_budget_transactions_path(@category.id)
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get new_category_budget_transaction_path(@category.id)
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the new template" do
+      get new_category_budget_transaction_path(@category.id)
+      expect(response).to render_template("new")
+    end
+  end
 end
